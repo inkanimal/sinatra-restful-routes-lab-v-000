f062abb9ec200b39b53e04cacf85fc0f3fc79355
geoffrey woodcock <theatomproject@gmail.com>
2018-08-15 20:09:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
   
-  get '/redcipes/:id' do
+  get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
